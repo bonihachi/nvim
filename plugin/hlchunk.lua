16ad636219deaf2719c427f0577fabaeddc3f1ad
bonihachi <tmurono@outlook.com>
2024-01-13 12:02:58 +0900

    modified hlchunk config

--- plugin/hlchunk.lua
+++ plugin/hlchunk.lua
@@ -1,12 +1,32 @@
 require('hlchunk').setup({
-    indent = {
-        chars = { "│", "¦", "┆", "┊", }, -- more code can be found in https://unicodeplus.com/
-
-        style = {
-            "#4C566A",
-        },
+  chunk = {
+    style = {
+      { fg = "#cdd1e6" },
+      { fg = "#c21f30" }, -- this fg is used to highlight wrong chunk
     },
-    blank = {
-        enable = false,
+  },
+  indent = {
+    enable = false,
+    chars = { "│", "¦", "┆", "┊", }, -- more code can be found in https://unicodeplus.com/
+
+    style = {
+      "#4C566A",
+    },
+  },
+  line_num = {
+    enable = true,
+    use_treesitter = false,
+    style = "#cdd1e6",
+  },
+
+  blank = {
+    enable = true,
+    chars = {
+      " ",
+    },
+    style = {
+      { bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("cursorline")), "bg", "gui") },
+      { bg = "",                                                                         fg = "" },
     }
+  }
 })
